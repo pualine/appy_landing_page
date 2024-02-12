b05a0ae83f89cf9020b5160a5840bd4092b1a145
pualine <prisypauline.com>
2024-02-12 07:05:08 -0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+console.log('We are Betta');
+
+var firstName = 'Ellah';
+var age = 23;
+
+
+let LastName = 'Whizzy';
+const bililionWaterTemperature =  100;
+
+// let name = 'Vince';
+ 
+if (LastName == 'Ellah') {
+    console.log('Please enter a new name');
+}
+else if (LastName != 'Ellah'){
+    console.log('Enter your first name');
+}
+else
+console.log('What is your first name?')
+
+
+// counter = 30;
+for( let i = 1; i <= 30; i++){
+    // console.log('Hi there! how is 2024 going so far?', i);
+}
+// console.log(counter);
+
+console.log(firstName);
+console.log(LastName);
+console.log(age);
